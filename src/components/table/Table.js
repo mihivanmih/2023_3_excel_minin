5af5e5636d9f7b199768ddc3c473aec15ffe4677
Gusb <mihivanmih@gmail.com>
2023-03-26 22:35:39 +0300

    lesson 6 One Store

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -49,10 +49,19 @@ export class Table extends ExcelComponent {
         this.$dispatch({type: 'TEST'})
         this.$emit('table_select', $cell)
     }
+    
+    async resizeTable(event) {
+        try {
+            const data = await resizeHandler(this.$root, event)
+            this.$dispatch({type: 'TABLE_RESIZE', data})
+        } catch (e) {
+            console.log('resizeTable', e)
+        }
+    }
 
     onMousedown(event) {
         if(shouldResize(event)) {
-            resizeHandler(this.$root, event)
+            this.resizeTable(event)
         } else if (isCell(event)) {
             const $target = $(event.target)
             if(event.shiftKey) {

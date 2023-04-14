4174f766f5b167499817f701b6b20dc2c8bdc1e9
Gusb <mihivanmih@gmail.com>
2023-04-14 10:55:08 +0300

    lesson 7 routing

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import './scss/index.scss'
-import { Excel } from './components/excel/Excel'
-import { Header } from './components/header/Header'
-import { Toolbar } from './components/toolbar/Toolbar'
-import { Formula } from './components/formula/Formula'
-import { Table } from './components/table/Table'
-import { createStore } from './core/createStore'
-import { rootReduser } from './redux/rootReduser'
-import { debounce, storage } from './core/utils'
-import { initialState } from './redux/initialState'
+import { Router } from './core/routes/Router'
+// import { Excel } from './components/excel/Excel'
+// import { Header } from './components/header/Header'
+// import { Toolbar } from './components/toolbar/Toolbar'
+// import { Formula } from './components/formula/Formula'
+// import { Table } from './components/table/Table'
+// import { createStore } from './core/createStore'
+// import { rootReduser } from './redux/rootReduser'
+// import { debounce, storage } from './core/utils'
+// import { initialState } from './redux/initialState'
+//
+// const store = createStore(rootReduser, initialState)
+//
+// const stateListener = debounce(state => {
+//     console.log("state", state)
+//     storage('excel-state', state)
+// }, 300)
+//
+// store.subscribe(stateListener)
+//
+// const excel = new Excel('#app', {
+//     components: [Header, Toolbar, Formula, Table],
+//     store
+// })
+//
+// excel.render()
 
-const store = createStore(rootReduser, initialState)
+new Router('#app', {
 
-const stateListener = debounce(state => {
-    console.log("state", state)
-    storage('excel-state', state)
-}, 300)
-
-store.subscribe(stateListener)
-
-const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table],
-    store
 })
-
-excel.render()
